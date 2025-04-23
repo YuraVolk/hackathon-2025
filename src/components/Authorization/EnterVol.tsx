19b20b44338d298c9a0382c45e8d5fdff68cd871
lkjhgtlopikl <d3tomaso@yandex.com>
2025-04-23 23:30:37 +0300

    валидация формы регистрации и входа для волонтера

--- src/components/Authorization/EnterVol.tsx
+++ src/components/Authorization/EnterVol.tsx
@@ -2,28 +2,62 @@ import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Paper from "@mui/material/Paper";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import FilledInput from "@mui/material/FilledInput";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
-interface SignProps {
+interface EnterProps {
   num?: number;
   head?: string;
   des?: string;
 }
 
-const Sign: React.FC<SignProps> = (props) => {
+const Enter: React.FC<EnterProps> = (props) => {
+  const [showPassword, setShowPassword] = React.useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const [hasErr, setHasErr] = useState<boolean>(false);
+  const [errors, setErrors] = useState({
+    email: false,
+    password: false,
+  });
+  const enter = async (email: string, password: string) => {
+    const newErrors = { ...errors };
+    if (email == "") {
+      newErrors.email = true;
+    }
+    if (isNaN(Number(password))) {
+      newErrors.password = true;
+    }
+    if (!newErrors.email && !newErrors.password) {
+      const data = { email, password };
+      try {
+        const response = fetch("", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
 
-  const handleSendEmail = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const value = e.currentTarget.dataset.value || "";
-    setEmail(value);
-    setHasErr(value.trim().length === 0);
-  };
+        if (!(await response).ok) {
+          throw new Error("Ошибка при отправке данных");
+        }
 
-  const handleSendPass = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const value = e.currentTarget.dataset.value || "";
-    setPassword(value);
-    setHasErr(value.trim().length === 0);
+        const result = (await response).json();
+        console.log("Успех:", result);
+      } catch (error) {
+        console.error("Ошибка:", error);
+      }
+
+      setPassword("");
+      setEmail("");
+    }
+    setErrors(newErrors);
   };
 
   return (
@@ -31,30 +65,63 @@ const Sign: React.FC<SignProps> = (props) => {
       <form className="addVolForm">
         <h2>Войти</h2>
         <TextField
-          className="email"
           fullWidth
-          id="email"
-          label="Ваша почта"
-          variant="filled"
-          style={{ border: hasErr ? "1px solid red" : undefined }}
-          value={email}
-          onChange={(e) => setEmail(e.currentTarget.value)}
-        />
-        <TextField
           className="input"
-          fullWidth
-          id="password"
-          label="Ваш код доступа"
+          id="email"
+          label="Электронна почта"
           variant="filled"
-          style={{ border: hasErr ? "1px solid red" : undefined }}
-          value={password}
-          onChange={(e) => setPassword(e.currentTarget.value)}
+          error={errors.email}
+          helperText={errors.email ? "Введите вашу почту" : ""}
+          value={email}
+          onChange={(e) => {
+            setEmail(e.currentTarget.value);
+            setErrors({
+              email: false,
+              password: false,
+            });
+          }}
         />
+        <FormControl sx={{ m: 1, width: "100%" }} variant="filled">
+          <InputLabel htmlFor="filled-adornment-password">
+            {errors.password
+              ? "Иедентификатор состоит из цифр!"
+              : "Уникальный идентификатор"}
+          </InputLabel>
+          <FilledInput
+            error={errors.password}
+            id="filled-adornment-password"
+            type={showPassword ? "text" : "password"}
+            fullWidth
+            value={password}
+            onChange={(e) => {
+              setPassword(e.currentTarget.value);
+              setErrors({
+                email: false,
+                password: false,
+              });
+            }}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label={
+                    showPassword ? "hide the password" : "display the password"
+                  }
+                  onClick={handleClickShowPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
         <Button
           className="button"
-          disabled={hasErr}
+          disabled={errors.email || errors.password}
           color="secondary"
-          onClick={handleSendPass}
+          onClick={() => {
+            enter(email, password);
+          }}
           variant="contained"
         >
           Зарегистрироваться
@@ -64,4 +131,4 @@ const Sign: React.FC<SignProps> = (props) => {
   );
 };
 
-export default Sign;
+export default Enter;
--- src/components/Authorization/SignVol.tsx
+++ src/components/Authorization/SignVol.tsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Paper from "@mui/material/Paper";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { Dayjs } from "dayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 interface SignProps {
   num?: number;
@@ -15,30 +19,27 @@ const Sign: React.FC<SignProps> = (props) => {
   const [inn, setInn] = useState<string>("");
   const [phone_number, setPhoneNumber] = useState<string>("");
   const [email, setEmail] = useState<string>("");
-  const [birth_date, setBirthDate] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [birth_date, setBirthDate] = useState<Dayjs | null>(null);
   const [errors, setErrors] = useState({
     fio: false,
     inn: false,
     phone_number: false,
     email: false,
     birth_date: false,
-    password: false,
   });
 
-  const send = (
+  const send = async (
     fio: string,
     inn: string,
     phone_number: string,
     email: string,
-    birth_date: string,
-    password: string
+    birth_date: Dayjs | null
   ) => {
     const newErrors = { ...errors };
     if (fio == "") {
       newErrors.fio = true;
     }
-    if (inn == "") {
+    if (inn == "" || inn.length != 12) {
       newErrors.inn = true;
     }
     if (phone_number == "") {
@@ -47,115 +48,173 @@ const Sign: React.FC<SignProps> = (props) => {
     if (email == "") {
       newErrors.email = true;
     }
-    if (birth_date == "") {
+    if (birth_date == null) {
       newErrors.birth_date = true;
     }
-    if (isNaN(Number(password))) {
-      newErrors.password = true;
-    }
+    if (
+      !newErrors.birth_date &&
+      !newErrors.email &&
+      !newErrors.fio &&
+      !newErrors.inn &&
+      !newErrors.phone_number
+    ) {
+      const data = { fio, inn, phone_number, email, birth_date };
+      try {
+        const response = fetch("", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
 
+        if (!(await response).ok) {
+          throw new Error("Ошибка при отправке данных");
+        }
+
+        const result = (await response).json();
+        console.log("Успех:", result);
+      } catch (error) {
+        console.error("Ошибка:", error);
+      }
+      setFio("");
+      setInn("");
+      setPhoneNumber("");
+      setBirthDate(null);
+      setEmail("");
+    }
     setErrors(newErrors);
   };
 
   return (
-    <Paper elevation={3} className="addVol">
-      <form className="addVolForm">
-        <h2>Регистрация Волонтера</h2>
-        <label htmlFor="fio"></label>
-        <TextField
-          fullWidth
-          className="input"
-          error={errors.fio}
-          helperText={errors.fio ? "Введите ваше ФИО" : ""}
-          id="fio"
-          label="ФИО"
-          variant="filled"
-          value={fio}
-          onChange={(e) => {
-            setFio(e.currentTarget.value);
-            setErrors({    fio: false,
-              inn: false,
-              phone_number: false,
-              email: false,
-              birth_date: false,
-              password: false, })
-          }}
-        />
-        <TextField
-          fullWidth
-          className="input"
-          id="inn"
-          label="ИНН"
-          variant="filled"
-          error={errors.inn}
-          helperText={errors.inn ? "Введите ваш ИНН" : ""}
-          value={inn}
-          onChange={(e) => setInn(e.currentTarget.value)}
-        />
-        <TextField
-          fullWidth
-          className="input"
-          id="phone_number"
-          label="Ваш номер телефона"
-          variant="filled"
-          error={errors.phone_number}
-          helperText={errors.phone_number ? "Введите ваш номер телефона" : ""}
-          value={phone_number}
-          onChange={(e) => setPhoneNumber(e.currentTarget.value)}
-        />
-        <TextField
-          fullWidth
-          className="input"
-          id="birth_date"
-          label="Ваша дата рождения"
-          variant="filled"
-          error={errors.birth_date}
-          helperText={errors.birth_date ? "Введите вашу дату рождения" : ""}
-          value={birth_date}
-          onChange={(e) => setBirthDate(e.currentTarget.value)}
-        />
-        <TextField
-          fullWidth
-          className="input"
-          id="email"
-          label="Ваша почта"
-          variant="filled"
-          error={errors.email}
-          helperText={errors.email ? "Введите вашу почту" : ""}
-          value={email}
-          onChange={(e) => setEmail(e.currentTarget.value)}
-        />
-        <TextField
-          fullWidth
-          className="input"
-          id="password"
-          label="Ваш код доступа"
-          variant="filled"
-          error={errors.password}
-          helperText={errors.password ? "Введите ваш код доступа" : ""}
-          value={password}
-          onChange={(e) => setPassword(e.currentTarget.value)}
-        />
-        <Button
-          className="button"
-          disabled={
-            errors.birth_date ||
-            errors.email ||
-            errors.fio ||
-            errors.inn ||
-            errors.password ||
-            errors.phone_number
-          }
-          color="secondary"
-          onClick={() => {
-            send(fio, inn, phone_number, email, birth_date, password);
-          }}
-          variant="contained"
-        >
-          Зарегистрироваться
-        </Button>
-      </form>
-    </Paper>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Paper elevation={3} className="addVol">
+        <form className="addVolForm">
+          <h2>Регистрация Волонтера</h2>
+          <label htmlFor="fio"></label>
+          <TextField
+            fullWidth
+            className="input"
+            error={errors.fio}
+            helperText={errors.fio ? "Введите ваше ФИО" : ""}
+            id="fio"
+            label="ФИО"
+            variant="filled"
+            value={fio}
+            onChange={(e) => {
+              setFio(e.currentTarget.value);
+              setErrors({
+                fio: false,
+                inn: false,
+                phone_number: false,
+                email: false,
+                birth_date: false,
+              });
+            }}
+          />
+          <TextField
+            fullWidth
+            className="input"
+            id="inn"
+            label="ИНН"
+            variant="filled"
+            error={errors.inn}
+            helperText={errors.inn ? "Введите ваш ИНН" : ""}
+            value={inn}
+            onChange={(e) => {
+              setInn(e.currentTarget.value);
+              setErrors({
+                fio: false,
+                inn: false,
+                phone_number: false,
+                email: false,
+                birth_date: false,
+              });
+            }}
+          />
+          <TextField
+            fullWidth
+            className="input"
+            id="phone_number"
+            label="Номер телефона"
+            variant="filled"
+            error={errors.phone_number}
+            helperText={errors.phone_number ? "Введите ваш номер телефона" : ""}
+            value={phone_number}
+            onChange={(e) => {
+              setPhoneNumber(e.currentTarget.value);
+              setErrors({
+                fio: false,
+                inn: false,
+                phone_number: false,
+                email: false,
+                birth_date: false,
+              });
+            }}
+          />
+          <DatePicker
+            label="Дата рождения"
+            value={birth_date}
+            onChange={(date: Dayjs | null) => {
+              setBirthDate(date);
+              setErrors({ ...errors, birth_date: date === null });
+            }}
+            slotProps={{
+              openPickerIcon: { fontSize: "large" },
+              openPickerButton: {
+                color: errors.birth_date ? "error" : "undefind",
+              },
+              textField: {
+                variant: "filled",
+                focused: true,
+                fullWidth: true,
+                color: errors.birth_date ? "error" : "#1e1e1e",
+              },
+            }}
+          />
+          {errors.birth_date && (
+            <div style={{ color: "red" }}>Введите вашу дату рождения</div>
+          )}
+          <TextField
+            fullWidth
+            className="input"
+            id="email"
+            label="Электронна почта"
+            variant="filled"
+            error={errors.email}
+            helperText={errors.email ? "Введите вашу почту" : ""}
+            value={email}
+            onChange={(e) => {
+              setEmail(e.currentTarget.value);
+              setErrors({
+                fio: false,
+                inn: false,
+                phone_number: false,
+                email: false,
+                birth_date: false,
+              });
+            }}
+          />
+          <Button
+            className="button"
+            disabled={
+              errors.birth_date ||
+              errors.email ||
+              errors.fio ||
+              errors.inn ||
+              errors.phone_number
+            }
+            color="secondary"
+            onClick={() => {
+              send(fio, inn, phone_number, email, birth_date);
+            }}
+            variant="contained"
+          >
+            Зарегистрироваться
+          </Button>
+        </form>
+      </Paper>
+    </LocalizationProvider>
   );
 };
 
